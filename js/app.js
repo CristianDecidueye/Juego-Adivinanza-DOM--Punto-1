58f453fb96ba77515cf1c357924bf09a4685ab65
Cristian Diaz <diazcristian0210@gmail.com>
2024-02-29 16:10:12 -0300

    Cree la estructura del proyecto

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// ACA EL JUEGO SELECCIONA EL NUMERO AL AZAR
+let numeroAzar = Math.floor(Math.random()*100) + 1
+let N
+
+let numeroEntrada = document.getElementById('numeroEntrada')
+
+let mensaje = document.getElementById('mensaje')
+let intento = document.getElementById('intento')
+let intentos = 0
+
+
+// ESTA FUNCION SE VA A EJECUTAR CUANDO SE TOQUE EL BOTON CHEQUEAR
+function chequearResultado(){
+    intentos ++
+    intento.textContent = intentos 
+let numeroIngresado = parseInt(numeroEntrada.value)
+
+if(numeroIngresado <1 || isNaN (numeroIngresado)){
+    mensaje.textContent = 'Por favor, introduce un numero valido'
+return
+}
+if(numeroIngresado === numeroAzar){
+    mensaje.textContent = '¡FELICITACIONES HAZ ADIVINADO EL NUMERO';
+    mensaje.style.color = 'green';
+    numeroEntrada.disabled = true;
+}
+else if(numeroIngresado < numeroAzar){
+    mensaje.textContent = '¡Haz alto! El numero es mayor al que elegiste';
+    mensaje.style.color = 'red';
+}else{
+    mensaje.textContent = '¡Haz mas bajo! El numero es menor al que elegiste';
+    mensaje.style.color = 'orange';
+}
+}
